9b4ac2dee35f6bda303eb6e6112f9c11caaa97de
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-05-22 01:46:21 +0600

    group-by-and-having

---
+++ groupBy.sql
@@ -0,0 +1,27 @@
+SELECT * from students;
+
+
+-- ** Group by
+--  SELECT country FROM students
+--    GROUP BY country; // for grouping output
+
+--  SELECT country, count(*) FROM students  // its count all data from selected column
+--    GROUP BY country;
+
+--  SELECT country, count(*), avg(age) FROM students
+--    GROUP BY country;// for finding avarage form different rows
+
+
+--  Having
+--  SELECT country, avg(age) FROM students
+--    GROUP BY country
+--       HAVING avg(age) > 20.60;
+
+
+-- SELECT extract(year from dob) as birth_year
+--    FROM students
+--       GROUP BY birth_year; 
+
+-- SELECT extract(year from dob) as birth_year, count(*)// its counting that how many row in same year
+--    FROM students
+--       GROUP BY birth_year;
