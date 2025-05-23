a085392f89a484884d0655d6cd72db66edf527fb
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-05-23 23:12:50 +0600

    inner-join

---
+++ join.sql
@@ -0,0 +1,64 @@
+--  create a user table
+CREATE TABLE "user"(id SERIAL PRIMARY KEY, username VARCHAR(25)NOT NULL);
+
+-- create a post table incuding foreign key user id table relation
+CREATE TABLE post(
+     id SERIAL PRIMARY KEY,
+     title text NOT NULL,
+     user_id INTEGER REFERENCES "user"(id)  on DELETE SET DEFAULT DEFAULT 2
+    --  //foreign key
+);
+
+
+INSERT INTO "user" (username) VALUES
+('akash'),('batash'),('sagor'),('nodi');
+
+SELECT * FROM "user";
+
+INSERT INTO post (title, user_id)
+VALUES 
+  ('Getting Started with PostgreSQL', 1),
+  ('Understanding SQL Joins', 2),
+  ('PostgreSQL Performance Tips', 3),
+  ('How to Use Indexes Effectively', 1),
+  ('Backup and Restore in PostgreSQL',2);
+
+  SELECT * FROM post; 
+
+
+
+
+
+
+
+-- ** inner join
+-- join keyword for joining multiple table  
+
+
+
+
+-- --  full sytex -- with join relation with two table
+
+-- SELECT post.id, "user".username
+-- FROM post
+-- JOIN "user" ON post.user_id = "user".id;
+
+
+--   ** renameing table name like that
+-- SELECT p.id FROM post p
+-- JOIN "user" ON p.user_id = "user".id;
+-- 
+-- SELECT p.id, u.username FROM post p
+-- JOIN "user" u ON p.user_id = u.id;
+
+--   **also  renameing table name like that using as
+-- SELECT * FROM post as p
+-- JOIN "user" as u ON p.user_id = u.id;
+
+
+
+
+
+
+
+
